a2ceef1a2d8c618046cea6bd2c9fda71c1a78693
Scarain <412418836@qq.com>
2016-07-04 16:19:30 +0800

    Add files via upload

--- plot3.R
+++ plot3.R
@@ -24,7 +24,7 @@ png(filename = "./Plots/plot3.png", width = 480, height = 480, units="px")
 plot(data$DateTime, data$Sub_metering_1, xlab = "", ylab = "Energy sub metering", type = "l")
 lines(data$DateTime, data$Sub_metering_2, col = "red")
 lines(data$DateTime, data$Sub_metering_3, col = "blue")
-legend("topright", lty = 1, col = c("black","red","blue"), legend = c("Sub_metering_1","Sub_metering_2","Sub_netering_3"))
+legend("topright", lty = 1, col = c("black","red","blue"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 
 dev.off()
 
